6d5a6ed99f0632bb0f0c7edf3e91fa36b61477b0
charliemacquarie <7698633+charliemacquarie@users.noreply.github.com>
2026-02-26 10:07:12 -0800

    combine flash and morse

--- cw.py
+++ cw.py
@@ -3,12 +3,17 @@ import time
 from gpiod import request_lines, LineSettings
 from gpiod.line import Direction, Value
 
+from continuous import make_morse
+
+# MORSE TIMINGS
 dot = 0.5
 dash = (dot * 3)
-pause = dot
-letter = (dot * 3)
+pause = dash
 space = (dot * 7)
 
+TEST_STRING = 'KN6HLC sending 73s!'
+
+morse = make_morse(TEST_STRING)
 
 LINE = 91
 
@@ -18,15 +23,20 @@ with request_lines(
     config={
         LINE: LineSettings(
             direction=Direction.OUTPUT, output_value=Value.ACTIVE
-        )
-    },
-) as request:
-    while True:
-        request.set_value(LINE, Value.ACTIVE)
-        time.sleep(dot)
-        request.set_value(LINE, Value.INACTIVE)
-        time.sleep(pause)
-        request.set_value(LINE, Value.ACTIVE)
-        time.sleep(dash)
-        request.set_value(LINE, Value.INACTIVE)
+            )
+        },
+    ) as request:
+        print(morse)
+        for i in morse:
+            if i == '.':
+                request.set_value(LINE, Value.ACTIVE)
+                time.sleep(dot)
+            if i == '-':
+                request.set_value(LINE, Value.ACTIVE)
+                time.sleep(dash)
+            if i == ' ':
+                request.set_value(LINE, Value.INACTIVE)
+                time.sleep(pause)
 
+            request.set_value(LINE, Value.INACTIVE)
+            time.sleep(dot)
