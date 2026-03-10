bf5db2c603a084f60243119d7ae1bfb4a3d2794b
charliemacquarie <7698633+charliemacquarie@users.noreply.github.com>
2026-03-10 09:25:40 -0700

    include idle

--- cw.py
+++ cw.py
@@ -3,7 +3,7 @@ import time
 from gpiod import request_lines, LineSettings
 from gpiod.line import Direction, Value
 
-from continuous import make_morse, blink_morse
+from continuous import make_morse, blink_morse, blink_idle
 
 # MORSE TIMINGS
 dot = 0.5
@@ -18,29 +18,5 @@ morse = make_morse(TEST_STRING)
 LINE = 91
 
 blink_morse(91, morse)
-"""
-# REPLACE this part with the actual function and check it works...
-with request_lines(
-    "/dev/gpiochip1",
-    consumer="blink-example",
-    config={
-        LINE: LineSettings(
-            direction=Direction.OUTPUT, output_value=Value.ACTIVE
-            )
-        },
-    ) as request:
-        for i in morse:
-            if i == '.':
-                request.set_value(LINE, Value.ACTIVE)
-                time.sleep(dot)
-            if i == '-':
-                request.set_value(LINE, Value.ACTIVE)
-                time.sleep(dash)
-            if i == ' ':
-                request.set_value(LINE, Value.INACTIVE)
-                time.sleep(pause)
 
-            request.set_value(LINE, Value.INACTIVE)
-            time.sleep(dot)
-
-"""
+blink_idle(91)
