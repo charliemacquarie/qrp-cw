6caeed8ac2479456efa18289f2270a257cef7532
charliemacquarie <7698633+charliemacquarie@users.noreply.github.com>
2026-03-21 16:02:09 -0700

    just commenting

--- cw.py
+++ cw.py
@@ -15,9 +15,15 @@ TEST_STRING = 'KN6HLC sending 73s!'
 
 morse = make_morse(TEST_STRING)
 
+# gpio lines right now should be:
+# 91, 92, 94
+
 LINE = 92
 cycle = 10
 
+
+# NOW come back in and make a loop that actually looks in the dirs and
+# goes to idle if nothing.
 blink_morse(LINE, morse)
 
 blink_idle(LINE, cycle)
